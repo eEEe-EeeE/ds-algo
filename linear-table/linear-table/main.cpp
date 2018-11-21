5352c0410522f1ff2f0a0c2db9a426bc7cc991b8
C J <hiddenbee@163.com>
2018-11-21 21:41:34 +0800

    add something

---
+++ linear-table/linear-table/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+int main() {
+	cout << "linear practice." << endl;
+	exit(0);
+}
