a061b156914e7626fb19982ec8a368cd06c1a3c7
C J <civilian_x@163.com>
2019-03-15 00:27:09 +0800

    add

--- test/test/Stack.h
+++ test/test/Stack.h
@@ -5,5 +5,24 @@ private:
 	T list[SIZE];
 	int top;
 public:
-
+	bool isEmpty() { return top == -1 ? true : false; }
+	bool isFull() { return top == SIZE - 1 ? true : false; }
+	void push(const T & item);
+	T pop();
+	void clear();
 };
+
+template <class T, int SIZE>
+void Stack<T, SIZE>::push(const T & item) {
+	
+}
+
+template <class T, int SIZE>
+T Stack<T, SIZE>::pop() {
+
+}
+
+template <class T, int SIZE>
+void Stack<T, SIZE>::clear() {
+
+}
